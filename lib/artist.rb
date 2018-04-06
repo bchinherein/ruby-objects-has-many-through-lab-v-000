4a67c24b488cd00f5da5a257dd1c16106327ea8e
Brandon Chin <brandonchin@me.com>
2018-04-06 17:00:59 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+require 'pry'
+class Artist
+  attr_accessor :name, :song, :genre
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def songs
+    @songs
+  end
+
+  def genres
+    songs.collect do |s|
+      s.genre.name
+      #binding.pry
+    end
+  end #def genres
+
+end #class Artist
---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+  attr_accessor :name, :genre, :artist
+
+  def initialize(name, genre)
+    @name = name
+    @genre = genre
+  end
+
+  #def genre= (genre)
+  #  self.genre = genre
+  #end
+
+end #class Song
